d0cd6d96a505e04d5e4ebf7cbd130c35ed8b91ae
ner1nal <evgechiklyk@gmail.com>
2023-10-09 23:39:26 +0700

    ver2.1

---
+++ Lab2/headers/Strings.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <ctime>
+
+#ifndef STRINGS_H
+#define STRINGS_H
+
+class Strings {
+
+private:
+    char* charArray;
+    int currSize;
+    tm* timeOfCreate;
+
+public:
+    Strings();
+    Strings(int, const char*);
+    Strings(const Strings&);
+    ~Strings();
+    int FindStr(const char*);
+    void ChangeStr(int, const char*);
+    void PrintStr();
+    void UnionStr(int, const char*);
+    void RegTime();
+
+    Strings operator + (const Strings& string);
+    friend Strings operator - (Strings& object1, const Strings& object2);
+};
+
+
+#endif
---
+++ Lab2/main.cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+#include <cstring>
+#include "D:\Labs C++\labs\Lab2\headers\Strings.h"
+
+int main()
+{
+    std::cout << "Parameterized constructor:" << std::endl;
+    const char* str1 = "Hello, world";
+    Strings object(strlen(str1), str1);
+    object.PrintStr();
+    std::cout << std::endl;
+
+    std::cout << "Changing values:" << std::endl;
+    const char* str2 = "Hello, bob";
+    object.ChangeStr(strlen(str2), str2);
+    object.PrintStr();
+    std::cout << std::endl;
+
+    std::cout << "Copy constructor:" << std::endl;
+    Strings object1(object);
+    object1.PrintStr();
+    std::cout << std::endl;
+
+    std::cout << "Default constructor:" << std::endl;
+    Strings object2;
+    object2.PrintStr();
+    std::cout << std::endl;
+
+    std::cout << "Substring found at index " << object.FindStr("bob") << std::endl;
+    std::cout << std::endl;
+
+    std::cout << "Union strings:" << std::endl;
+    object.UnionStr(strlen(str1), str1);
+    object.PrintStr();
+    std::cout << std::endl;
+
+    std::cout << "+ overloading" << std::endl;
+    object2 + object1;
+    object2.PrintStr();
+    std::cout << std::endl;
+
+    const char* str3 = "Hello, world boba";
+    Strings object3(strlen(str3), str3);
+    const char* str4 = "world"; 
+    Strings object4(strlen(str4), str4);
+
+    std::cout << "- overloading:" << std::endl;
+    object3 - object4;
+    object3.PrintStr();
+    std::cout << std::endl;
+
+    return 0;
+}
